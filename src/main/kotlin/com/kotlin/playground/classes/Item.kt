6c853d810df8fa290b826965706060c6bc12b2a1
Tutku Ince <tutku.ince@outlook.com>
2024-06-13 23:37:35 +0300

    Custom Getters and Setters

--- src/main/kotlin/com/kotlin/playground/classes/Item.kt
+++ src/main/kotlin/com/kotlin/playground/classes/Item.kt
@@ -3,10 +3,26 @@ package com.kotlin.playground.classes
 class Item() {
     var name: String = ""
 
+    // When to use Custom Getter/Setter?
+    // Use it when you have the need to implement the custom logic for setting or retrieving the properties
+    var price: Double = 0.0
+        get() {
+            println("Inside getter")
+            return field
+        }
+        set(value) {
+            println("Inside setter")
+            if (value >= 0.0) {
+                field = value
+            } else {
+                throw IllegalArgumentException("Negative Price is not allowed!")
+            }
+        }
+
     // Recommended approach for constructors
     // Use Primary Constructors whenever possible
     // Use default values for overloaded constructor scenario
-    constructor(_name: String): this() {
+    constructor(_name: String) : this() {
         name = _name
     }
 }
@@ -19,4 +35,9 @@ fun main() {
 
     val person: Person = Person(_email = "tutku@mail.com")
     println("Name: ${person.name} and the age is ${person.age} and the email is ${person.email}")
+    println("--------GETTER&SETTER--------")
+
+    item.price = 1.1
+    println("Item price is ${item.price}")
+
 }
