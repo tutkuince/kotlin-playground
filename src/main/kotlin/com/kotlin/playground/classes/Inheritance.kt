e4d66079aa846b8944a2d5e6bf2b90cd43ea9ae9
Tutku Ince <tutku.ince@outlook.com>
2024-06-14 00:09:38 +0300

    Inheritance - Override Functions, Variables

--- src/main/kotlin/com/kotlin/playground/classes/Inheritance.kt
+++ src/main/kotlin/com/kotlin/playground/classes/Inheritance.kt
@@ -14,10 +14,19 @@ package com.kotlin.playground.classes
  * class Student(name: String): User(name)
  * */
 
+// Overriding Functions -> Mark the function with "open" modifier
+// Overriding Variables -> Similar to functions using the open and override keywords
+
 // Allows inheritance with open modifier
 open class User(val name: String) {
+    open var isLoggedOut: Boolean = false
+
     fun login() {
         println("Inside user Login")
+    }
+
+    open fun logout() {
+        println("Inside user Logout")
     }
 }
 
@@ -25,14 +34,29 @@ open class Staff(val id: Int) {}
 // Only one class may appear in a supertype list
 // class Cleaner(name: String): User(name), Staff() {}
 
-class Student(name: String): User(name) {}
+class Student(name: String): User(name) {
+    override var isLoggedOut: Boolean = false
+    override fun logout() {
+        super.logout()
+        println("Inside Student Logout")
+    }
+}
 
-class Instructor(name: String): User(name) {}
+class Instructor(name: String): User(name) {
+    override var isLoggedOut: Boolean = true
+    override fun logout() {
+        println("Inside Instructor Logout")
+        super.logout()
+    }
+}
 
 fun main() {
     val student = Student("Tutku")
     println("Name is ${student.name}")
     student.login()
+    student.logout()
+    student.isLoggedOut = true
+    println("Is Student Logged Out: ${student.isLoggedOut}")
 
     val instructor = Instructor("Çiğdem")
     println("Name is ${instructor.name}")
