83edce67656c3bbc9b1d11044e3bf76014683a6d
Tutku Ince <tutku.ince@outlook.com>
2024-06-13 13:26:25 +0300

    primary constructors

--- src/main/kotlin/com/kotlin/playground/classes/Person.kt
+++ src/main/kotlin/com/kotlin/playground/classes/Person.kt
@@ -1,6 +1,11 @@
 package com.kotlin.playground.classes
 
-class Person {
+class Person(
+    val name: String = "Name",
+    val age: Int = 0
+) {
+
+
 
     // Class in object oriented programming is fundamentally the blueprint for creating objects
     fun action() {
@@ -9,6 +14,10 @@ class Person {
 }
 
 fun main() {
-    val person: Person = Person()
+    val person: Person = Person("Tutku", 35)
     person.action()
+    println("Name: ${person.name} and the age is ${person.age}")
+
+    val person1: Person = Person()
+    println("Name: ${person1.name} and the age is ${person1.age}")
 }
