07bb205e6eeb708346da24997d29feaacd7366d3
Tutku Ince <tutku.ince@outlook.com>
2024-06-14 16:52:35 +0300

    Filter Operations on Kotlin Collections

---
+++ src/main/kotlin/com/kotlin/playground/collections/CollectionOperations.kt
@@ -0,0 +1,23 @@
+package com.kotlin.playground.collections
+
+import com.kotlin.playground.dataset.Course
+import com.kotlin.playground.dataset.CourseCategory
+import com.kotlin.playground.dataset.courseList
+
+fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+    val developmentCourses = courseList
+        //.filter { it.category == CourseCategory.DEVELOPMENT }
+        .filter{ predicate.invoke(it) }
+        .forEach { println("Course: $it") }
+}
+
+fun main() {
+    val courseList = courseList()
+    val devPredicate = { course: Course -> course.category == CourseCategory.DEVELOPMENT }
+    val desPredicate = { course: Course -> course.category == CourseCategory.DESIGN }
+    exploreFilter(courseList, devPredicate)
+    println("---DESIGN-COURSES---")
+    exploreFilter(courseList, desPredicate)
+
+}
+
