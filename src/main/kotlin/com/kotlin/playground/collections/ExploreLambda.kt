75323c3f6c802f2efe5d92d5b7aa13bd50cc5c96
Tutku Ince <tutku.ince@outlook.com>
2024-06-14 16:35:21 +0300

    Lambdas and Higher Order Functions

--- src/main/kotlin/com/kotlin/playground/collections/ExploreLambda.kt
+++ src/main/kotlin/com/kotlin/playground/collections/ExploreLambda.kt
@@ -1,5 +1,10 @@
 package com.kotlin.playground.collections
 
+// Higher Order Function
+fun calculate(x: Int, y: Int, op: (x: Int, y: Int) -> Int): Int {
+    return op(x, y)
+}
+
 fun main() {
     val addLambda = { x: Int -> x + x }
     val addResult = addLambda(3)
@@ -11,4 +16,10 @@ fun main() {
     }
     val multiplyResult = multiplyLambda(3, 5)
     println("Multiply Result is $multiplyResult")
+
+    val multiplyCalculateResult = calculate(5, 4) { x, y -> x * y }
+    println("Multiply Calculate Result is $multiplyCalculateResult")
+
+    val addCalculateResult = calculate(5, 4, { x, y -> x + y })
+    println("Add Calculate Result is $addCalculateResult")
 }
