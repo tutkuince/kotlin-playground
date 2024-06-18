02834ed23a5b0d481c379845f1ad5dcb0bab5d04
Tutku Ince <tutku.ince@outlook.com>
2024-06-18 22:42:23 +0300

    map operator with kotlin

--- src/main/kotlin/com/kotlin/playground/collections/CollectionOperations.kt
+++ src/main/kotlin/com/kotlin/playground/collections/CollectionOperations.kt
@@ -7,8 +7,14 @@ import com.kotlin.playground.dataset.courseList
 fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
     val developmentCourses = courseList
         //.filter { it.category == CourseCategory.DEVELOPMENT }
-        .filter{ predicate.invoke(it) }
+        .filter { predicate.invoke(it) }
         .forEach { println("Course: $it") }
+}
+
+fun exploreMap(courseList: MutableList<Course>) {
+    val courses = courseList
+        .map { "${it.category.name.uppercase()} - ${it.name.uppercase()}" }
+    println("Courses: $courses")
 }
 
 fun main() {
@@ -17,7 +23,8 @@ fun main() {
     val desPredicate = { course: Course -> course.category == CourseCategory.DESIGN }
     exploreFilter(courseList, devPredicate)
     println("---DESIGN-COURSES---")
-    exploreFilter(courseList, desPredicate)
-
+    // exploreFilter(courseList, desPredicate)
+    exploreMap(courseList)
 }
+
 
