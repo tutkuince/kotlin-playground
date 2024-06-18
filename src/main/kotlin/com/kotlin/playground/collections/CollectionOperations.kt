94439cf02e6e7fd59871903eff6ec116fbaf1ed3
Tutku Ince <tutku.ince@outlook.com>
2024-06-18 23:10:01 +0300

    FlatMap Operations in Collections

--- src/main/kotlin/com/kotlin/playground/collections/CollectionOperations.kt
+++ src/main/kotlin/com/kotlin/playground/collections/CollectionOperations.kt
@@ -2,7 +2,9 @@ package com.kotlin.playground.collections
 
 import com.kotlin.playground.dataset.Course
 import com.kotlin.playground.dataset.CourseCategory
+import com.kotlin.playground.dataset.KAFKA
 import com.kotlin.playground.dataset.courseList
+import com.kotlin.playground.functions.courseName
 
 fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
     val developmentCourses = courseList
@@ -19,6 +21,18 @@ fun exploreMap(courseList: MutableList<Course>) {
     println("Courses: $courses")
 }
 
+// flatMap
+// This operators can be used if the collection has another collection
+// Used to flatten the list of lists that is operating on and returns the result as a single list
+fun exploreFlatMap(courseList: MutableList<Course>, kafka: String): List<String> {
+    val kafkaCourses = courseList.flatMap { course ->
+        val courseName = course.name
+        course.topicsCovered.filter { it == kafka }
+            .map { courseName }
+    }
+    return kafkaCourses
+}
+
 fun main() {
     val courseList = courseList()
     val devPredicate = { course: Course -> course.category == CourseCategory.DEVELOPMENT }
@@ -26,7 +40,18 @@ fun main() {
     exploreFilter(courseList, devPredicate)
     println("---DESIGN-COURSES---")
     // exploreFilter(courseList, desPredicate)
-    exploreMap(courseList)
+    // exploreMap(courseList)
+    val kafkaCourses = exploreFlatMap(courseList, KAFKA)
+    println("Kafka Courses: $kafkaCourses")
+
+    val list = listOf(listOf(1, 2, 3), listOf(4, 5, 6))
+    val mapResult = list
+        .map { outerList ->
+            outerList.map { it.toDouble() }
+        }
+    println("MapResult: $mapResult")            // [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
+
+    val flatMapResult = list
+        .flatMap { outerList -> outerList.map { it.toDouble() } }
+    println("FlatMapResult: $flatMapResult")    // [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
 }
-
-
